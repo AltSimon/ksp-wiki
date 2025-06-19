59e2c39bc91a7578498eacee2dea1b17f805aaa2
Simon Leneveu <simon.leneveu@gmail.com>
2025-06-19 02:01:11 +0200

    creating main

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello, world")
+
+	w.SetContent(widget.NewLabel("Test"))
+
+	w.ShowAndRun()
+}
